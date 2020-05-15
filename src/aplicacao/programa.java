3539413e57339a5b68438da77fa16ef16a2b19bb
Henrique Moreira <henriquemoreiraa@gmail.com>
2020-05-15 19:20:39 -0300

    Leitura do volume

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -9,9 +9,18 @@ public class programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		int qntC;
+		double tm;
+		
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		
+		System.out.println();
+		System.out.print("Quantas compras o cliente fez no último ano? ");
+		qntC = sc.nextInt();
+		System.out.print("Qual o ticket médio? ");
+		tm = sc.nextDouble();
 		
 		sc.close();
 	}

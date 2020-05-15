12f71a9e77109ef6cfc68326c8e2ce543ca66d16
Henrique Moreira <henriquemoreiraa@gmail.com>
2020-05-15 19:16:01 -0300

    Cabecalho

---
+++ src/aplicacao/programa.java
@@ -0,0 +1,19 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class programa {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("--------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		
+		sc.close();
+	}
+
+}
